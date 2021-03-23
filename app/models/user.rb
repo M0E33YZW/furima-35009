326a8eb14620066177173af534d1b3da4a44a93a
nyanmy <nyanmyth100@gmail.com>
2021-03-23 15:28:21 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :items
+  has_many :comments
+  has_many :orders
+       
+  validates :nickname,  presence: true
+  validates :lastname,  presence: true
+  validates :firstname, presence: true
+  validates :lastkana,  presence: true
+  validates :firstkana, presence: true
+  validates :birthday,  presence: true
 end
