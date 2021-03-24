30e7e1ffc3188187590a17314e5d3b2d9f3901d8
nyanmy <nyanmyth100@gmail.com>
2021-03-24 12:30:50 +0900

    FactoryBotの修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,16 @@
 FactoryBot.define do
   factory :user do
-    nickname  {Faker::Name.last_name}
+    transient do
+      person {Gimei.name}
+    end
+    nickname  {Faker::Name.name}
     email     {Faker::Internet.free_email}
-    password = Faker::Internet.password(min_length: 6)
-    password {password}
+    password  {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
-    lastname  {Faker::Japanese::Name.last_name}
-    firstname {Faker::Japanese::Name.first_name}
-    lastkana  {"カタカナ"}
-    firstkana {"カタカナ"}
-    birthday  {Faker::Date.birthday}
+    lastname  {person.last.kanji}
+    firstname {person.first.kanji}
+    lastkana  {person.last.katakana}
+    firstkana {person.first.katakana}
+    birthday  {Faker::Date.backward}
   end
 end
