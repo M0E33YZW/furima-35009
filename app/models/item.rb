8110b6cdc022f2d5a031768f4ab50e9b9c539ffc
nyanmy <nyanmyth100@gmail.com>
2021-03-25 17:02:33 +0900

    バリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,24 @@ class Item < ApplicationRecord
   belongs_to :user
   has_many :comments
   has_one :order
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping
+  belongs_to :area
+  belongs_to :day
+
+  with_options presence: true do
+    validates :title
+    validates :description
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :status_id
+      validates :shipping_id
+      validates :area_id
+      validates :day_id
+    end
+  end
 end
