0a1ba472a7634a218540ccbcacfef1d0685adb8f
nyanmy <nyanmyth100@gmail.com>
2021-03-27 00:17:42 +0900

    show,edit,updateアクションをコントローラーに定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,10 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:create, :new]
+  before_action :set_items, only: [:show, :edit, :update]
 
   def index
     @items = Item.all.order('created_at DESC')
+    @item = Item.new
   end
 
   def new
@@ -18,11 +20,30 @@ class ItemsController < ApplicationController
     end
   end
 
-  private
-  def edit
+  def show
+  end
 
+  def edit
+    unless current_user == @item.user
+      redirect_to root_path
+    end
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
+
+  private
   def item_params
     params.require(:item).permit(:image, :title, :description, :category_id, :status_id, :shipping_id, :area_id, :day_id,
                                  :price).merge(user_id: current_user.id)
+  end
+
+  def set_items
+    @item = Item.find(params[:id])
   end
 end
