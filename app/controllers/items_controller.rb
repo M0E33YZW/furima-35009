43171a72546bc344af2f28516a81bb6c52c514a9
nyanmy <nyanmyth100@gmail.com>
2021-03-27 10:47:31 +0900

    商品詳細表示機能のみの実装に修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,5 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:create, :new]
-  before_action :set_items, only: [:show, :edit, :update]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -21,18 +20,7 @@ class ItemsController < ApplicationController
   end
 
   def show
-  end
-
-  def edit
-    redirect_to root_path unless current_user == @item.user
-  end
-
-  def update
-    if @item.update(item_params)
-      redirect_to item_path
-    else
-      render :edit
-    end
+    @item = Item.find(params[:id])
   end
 
   private
@@ -40,9 +28,5 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :title, :description, :category_id, :status_id, :shipping_id, :area_id, :day_id,
                                  :price).merge(user_id: current_user.id)
-  end
-
-  def set_items
-    @item = Item.find(params[:id])
   end
 end
