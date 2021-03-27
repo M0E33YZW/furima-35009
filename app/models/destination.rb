67504266a042cf7c3d5ff6686479a38c97a22729
nyanmy <nyanmyth100@gmail.com>
2021-03-27 20:48:38 +0900

    バリデーションを設定

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,2 +1,11 @@
 class Destination < ApplicationRecord
+  belongs_to :order
+
+  with_options presence: true do
+    validates :order ,foreign_key: true
+    validates :zipcode
+    validates :area_id
+    validates :city
+    validates :address
+    validates :phone
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,8 @@
 class Order < ApplicationRecord
+  belongs_to :user
+  belongs_to :item
+  has_one    :destination
+  
   with_options presence: true, foreign_key: true do
     validates :user
     validates :item
