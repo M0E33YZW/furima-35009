46b65fac8955766780e59e20061a562e8b222c16
nyanmy <nyanmyth100@gmail.com>
2021-03-29 14:34:15 +0900

    バリデーションを編集

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,11 +1,3 @@
 class Destination < ApplicationRecord
   belongs_to :order
-
-  with_options presence: true do
-    validates :order ,foreign_key: true
-    validates :zipcode
-    validates :area_id
-    validates :city
-    validates :address
-    validates :phone
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,8 +2,4 @@ class Order < ApplicationRecord
   belongs_to :user
   belongs_to :item
   has_one    :destination
-  
-  with_options presence: true, foreign_key: true do
-    validates :user
-    validates :item
 end
