678fce218f037afc23c3f7a27333fe7f2fea4574
nyanmy <nyanmyth100@gmail.com>
2021-03-29 20:53:58 +0900

    バリデーションの修正

--- app/models/order_destination.rb
+++ app/models/order_destination.rb
@@ -2,22 +2,19 @@ class OrderDestination
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :zipcode, :area_id, :city, :address, :building, :phone, :token
 
-  with_options presence: true, foreign_key: true do
+  with_options presence: true do
     validates :user_id
     validates :item_id
-  end
-  with_options presence: true do
     validates :zipcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/ }
     validates :city
     validates :address
-    validates :phone, only_integer: true
+    validates :phone
     validates :token
   end
-  validates :area_id, numericality:  { other_than: 1 } do
+  validates :area_id, numericality: { other_than: 1 } 
 
-    def save
-      order = Order.create(user_id: user_id, item_id: item_id)
-      Destination.create(zipcode: zipcode, area_id: area_id, city: city, address: address, building: building, phone: phone, order_id: order.id)
-    end
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Destination.create(zipcode: zipcode, area_id: area_id, city: city, address: address, building: building, phone: phone, order_id: order.id)
   end
 end
