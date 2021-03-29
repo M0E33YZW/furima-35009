3d5c11faf63560f8c7695e9f996cf6e2287440d9
nyanmy <nyanmyth100@gmail.com>
2021-03-29 14:28:43 +0900

    ordersコントローラーの編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,15 +1,31 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, except: :index
+
   def index
+    @item = Item.find(params[:item_id])
   end
 
+  def new
+    @order_destination = OrderDestination.new
+  end
+  
   def create
-    @order = Order.new(order_params)
-    binding.pry
-    if @order.valid?
-      @order.save
-      return redirect_to root_path
+    @order_destination = OrderDestination.new(order_params)
+    if @order_destination.valid?
+      @order_destination.save
+      redirect_to root_path
     else
-      render 'index'
+      render :new
     end
   end
+
+  private
+
+  def order_params
+    params.require(:order_destination).permit(zipcode: zipcode, area_id: area_id, city: city, address: address, building: building, phone: phone).merge(user_id: current_user.id, item_id: item_id)
+  end
+
+  #def set_items
+    #@item = Item.find(params[:id])
+  #end
 end
