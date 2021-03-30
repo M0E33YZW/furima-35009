11eae1f5e932f77eb7c6131cfdc4b043d233e04a
nyanmy <nyanmyth100@gmail.com>
2021-03-30 15:49:52 +0900

    不要な記述の削除

--- spec/factories/order_destinations.rb
+++ spec/factories/order_destinations.rb
@@ -1,13 +1,11 @@
 FactoryBot.define do
   factory :order_destination do
-    token    { 'pk_test_b9c57876f0a97bf84eff3901' }
+    token    { ENV["PAYJP_PUBLIC_KEY"] }
     zipcode  { '123-4567' }
     area_id  { Faker::Number.between(from: 2, to: 48) }
     city     { '東京区' }
     address  { '東京777' }
     building { '東京ハイツ' }
     phone    { Faker::Number.number(digits: 11) }
-    association :user
-    association :item
   end
 end
