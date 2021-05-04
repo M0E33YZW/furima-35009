b267fb1d7be5be14da6fd6b28e9713b46711622f
nyanmy <nyanmyth100@gmail.com>
2021-05-04 21:39:53 +0900

    order_destinationモデルテストの期待するメッセージを日本語化

--- spec/models/order_destination_spec.rb
+++ spec/models/order_destination_spec.rb
@@ -23,52 +23,57 @@ RSpec.describe OrderDestination, type: :model do
       it '郵便番号が空では購入できない' do
         @order_destination.zipcode = nil
         @order_destination.valid?
-        expect(@order_destination.errors.full_messages).to include("Zipcode can't be blank")
+        expect(@order_destination.errors.full_messages).to include("郵便番号を入力してください")
       end
       it '郵便番号が半角のハイフンを含んだ正しい形式でないと購入できない' do
         @order_destination.zipcode = '1234567'
         @order_destination.valid?
-        expect(@order_destination.errors.full_messages).to include('Zipcode is invalid')
+        expect(@order_destination.errors.full_messages).to include('郵便番号は不正な値です')
       end
       it '都道府県の選択がなければ購入できない' do
         @order_destination.area_id = 1
         @order_destination.valid?
-        expect(@order_destination.errors.full_messages).to include('Area must be other than 1')
+        expect(@order_destination.errors.full_messages).to include('都道府県は1以外の値にしてください')
       end
       it '市区町村が空では登録できない' do
         @order_destination.city = ''
         @order_destination.valid?
-        expect(@order_destination.errors.full_messages).to include("City can't be blank")
+        expect(@order_destination.errors.full_messages).to include("市区町村を入力してください")
+      end
+      it '番地が空では登録できない' do
+        @order_destination.address = ''
+        @order_destination.valid?
+        expect(@order_destination.errors.full_messages).to include("番地を入力してください")
       end
       it '電話番号が空では登録できない' do
         @order_destination.phone = nil
         @order_destination.valid?
-        expect(@order_destination.errors.full_messages).to include("Phone can't be blank")
+        expect(@order_destination.errors.full_messages).to include("電話番号を入力してください")
       end
       it '電話番号が英数混合では登録できない' do
         @order_destination.phone = '0123456test'
         @order_destination.valid?
-        expect(@order_destination.errors.full_messages).to include('Phone is invalid')
+        expect(@order_destination.errors.full_messages).to include('電話番号は不正な値です')
       end
       it '電話番号が12桁以上では登録できない' do
         @order_destination.phone = '090123456789'
         @order_destination.valid?
-        expect(@order_destination.errors.full_messages).to include('Phone is invalid')
+        expect(@order_destination.errors.full_messages).to include('電話番号は不正な値です')
       end
       it 'tokenが空では登録できない' do
         @order_destination.token = nil
         @order_destination.valid?
-        expect(@order_destination.errors.full_messages).to include("Token can't be blank")
+        expect(@order_destination.errors.full_messages).to include("クレジットカード情報を入力してください")
       end
       it 'user_idが空では登録できない' do
         @order_destination.user_id = nil
         @order_destination.valid?
-        expect(@order_destination.errors.full_messages).to include("User can't be blank")
+        expect(@order_destination.errors.full_messages).to include("Userを入力してください")
       end
       it 'item_idが空では登録できない' do
         @order_destination.item_id = nil
         @order_destination.valid?
-        expect(@order_destination.errors.full_messages).to include("Item can't be blank")
+        expect(@order_destination.errors.full_messages).to include("Itemを入力してください")
       end
     end
   end
